80c9888ab8c37216e887651e5ec6377c6d044dee
fanta <472849306@qq.com>
2018-01-17 00:39:51 +0800

    180117

--- scripts/index.js
+++ scripts/index.js
@@ -11,6 +11,35 @@ $(document).ready(function(){
         $(this).css('color','gray');
         $('.loginOut').css('color','red');
     })
+
+    $('.gps-choice,.gps>li').hover(function(){
+        $('.gps').css('backgroundColor','#fff');
+        $('.gps').css('backgroundColor','rgb(227,228,229)');
+        $('.gps-choice').css('backgroundColor','#fff');
+        $('.gps-choice').css('border-left','.5px solid lightgray');
+        $('.gps-choice').css('border-right','.5px solid lightgray');
+        $('.gps>li').css('display','block');
+    },function(){
+        $('.gps').css('backgroundColor','rgb(227,228,229)');
+        $('.gps-choice').css('backgroundColor','rgb(227,228,229)');
+        $('.gps-choice').css('border-left','.5px solid transparent');
+        $('.gps-choice').css('border-right','.5px solid transparent');
+        $('.gps>li').css('display','none');
+    })
+
+   
+
+    $('.gps>li>span').hover(function(){
+        if($(this).hasClass('active')){
+            return false
+        }else{
+            $(this).css('color','red');
+            $(this).css('background-color','aliceblue');
+            }
+        },function(){
+            $(this).css('color','gray');
+            $(this).css('backgroundColor','');
+    })
    
 
 
